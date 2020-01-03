11652f639269b31405f07cf2626c3e3c4057141f
wonsokoh1019 <56017035+wonsokoh1019@users.noreply.github.com>
2020-01-03 19:59:39 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -1,3 +1,15 @@
+
+
+admin@lwbottest10 / works!23
+
+- 사내 문의 봇 배포
+- 근태 관리 봇 배포
+
+Rich Menu 배포하기
+
+
+Good 
+
 #!/bin/env python
 # -*- coding: utf-8 -*-
 import os
@@ -9,22 +21,25 @@ DEFAULT_LANG = 'ja'
 TZone = "Asia/Tokyo"
 
 # account
-ADMIN_ACCOUNT = "admin@xxx.com"
-DOMAIN_ID = 0
+ADMIN_ACCOUNT = "admin@lwbottest10"
+DOMAIN_ID = 154875
 
 # api
-API_ID = "kr1EHxxxxfJVz"
-SERVER_CONSUMER_KEY = "To8SnC7sxxxxxjqXZhO"
+API_ID = "kr1oOoAjxeykE"
+SERVER_CONSUMER_KEY = "RKA3u1efx_jYtgtHMBf7"
 
-SERVER_ID = "96460cc1e778402xxxxxfe35fa97ce76"
-SECRET_KEY_NAME = "private_201xxxxx164308.key"
+SERVER_ID = "b71da338e41b4ed0985c82d7478ae5fb"
+SECRET_KEY_NAME = "private_20200103132837.key"
 
 # LOCAL ADDRESS
-LOCAL_ADDRESS = "https://[Heroku app name].herokuapp.com/"
+LOCAL_ADDRESS = "https://attendancebot-test.herokuapp.com/"
 
 # DB config
-DB_HOST = "ec2-xxx-xxx-xxx-xxx.compute-1.amazonaws.com"
-DB_NAME = "x3mdxxxxsg5nx"
-DB_USER = "xsozxxxxvksikx"
+DB_HOST = "ec2-23-21-160-38.compute-1.amazonaws.com"
+DB_NAME = "d7dssp46qgen8l"
+DB_USER = "tbdxioxpsrxsnd"
 DB_PORT = "5432"
-DB_PASSWORD = "xxxxxb3ff80c85d331da880f4b3e0xxxxxxxxxxxd552d64d128317653ce0xxxx"
+DB_PASSWORD = "e6d9a9ee5db91ad04c9c09726d8ca598cfdfd99fd24d19b784679f7c02ea4602"
+
+
+ 
